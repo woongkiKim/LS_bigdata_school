deae1edff5fd51d3da729290282fa4b0e14b86bc
woongkiKim <50348532+woongkiKim@users.noreply.github.com>
2023-10-11 22:24:25 +0900

    외부 포트 열기

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def predict():
         return render_template("index.html", prediction=f"오류: {str(e)}")
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    app.run(debug=True)
+    app.run(host='0.0.0.0',  port=5001)
+    # app.run(debug=True)
